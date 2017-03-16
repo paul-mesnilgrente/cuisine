13ba84a3826190a6341ab07f3855537e8b9468e1
Paul Mesnilgrente <paul.mesnilgrente@gmail.com>
2017-03-16 17:37:19 +0100

    install mopa

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Craue\FormFlowBundle\CraueFormFlowBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
